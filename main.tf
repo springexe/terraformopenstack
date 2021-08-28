b2c13c10937208ead8694d9329fbf2f271ad5d25
springexe <64152489+springexe@users.noreply.github.com>
2021-08-29 02:35:28 +0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,3 @@
-terraform {
-required_version = ">= 0.14.0"
-  required_providers {
-    openstack = {
-      source  = "terraform-provider-openstack/openstack"
-      version = "~> 1.35.0"
-    }
-  }
-}
-
 provider "openstack" {
   user_name   = "admin"
   tenant_name = "admin"
